e31488b4397f668afbb8bfc7a0985cfcc993fd9a
Björn <b.kleipool@hotmail.com>
2023-11-13 22:33:36 +0100

    Added mnist test

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,32 @@
-use dfdx::{shapes::Const,
-    prelude::{LinearConfig, Tanh, Adam},
+use dfdx::{
+    prelude::{Adam, LinearConfig, Tanh},
+    shapes::Const,
     tensor::{Error, Tensor},
 };
 use dfdx_core::{
-    prelude::mse_loss,
     nn_traits::{BuildModuleExt, Module, Optimizer, ZeroGrads},
+    prelude::mse_loss,
+    shapes::Rank1,
     tensor::{AsArray, Trace},
-    tensor_ops::{Backward, AdamConfig}, shapes::Rank1,
+    tensor_ops::{AdamConfig, Backward},
 };
 use dfdx_derives::Sequential;
-
 
 //pub type Device = dfdx::tensor::Cpu;
 pub type Device = dfdx::tensor::Cuda;
 
 /// Autoencoder consisting of an encoder and decoder network
+///
+/// R = real-space vector size, L = latent-space vector size
 pub struct AutoEncoder<const R: usize, const L: usize> {
     encoder: MLP<R, L>,
     decoder: MLP<L, R>,
-    model: Option<(DeviceMLP<R, L, f32, Device>, DeviceMLP<L, R, f32, Device>)>
+    model: Option<(DeviceMLP<R, L, f32, Device>, DeviceMLP<L, R, f32, Device>)>,
 }
-
-
-/* 
-#[derive(Debug, Clone, Sequential)]
-pub struct AEConfig<const R: usize, const L: usize> {
-    pub encoder: MLP<R, L>,
-    pub decoder: MLP<L, R>,
-}
-*/
 
 /// Multi-layer perceptron with 1 hidden layer
+///
+/// I = input vector size, O = output vector size
 #[derive(Debug, Clone, Sequential)]
 pub struct MLP<const I: usize, const O: usize> {
     linear1: LinearConfig<Const<I>, usize>,
@@ -52,62 +48,22 @@ impl<const I: usize, const O: usize> MLP<I, O> {
     }
 }
 
-/*
-impl<const R: usize, const L: usize> AESequence<R, L> {
-    /// Return a model fitted to the input data
-    pub fn fit(&self, x: Tensor<(usize, Const<R>), f32, Device>) -> DeviceAESequence<R, L, f32, Device> {
-        // Initialise device
-        let dev: Device = Device::default();
-
-        // Define architecture
-        let arch: AESequence<R, L> = self.clone();
-        let mut model = dev.build_module::<f32>(arch);
-        let mut grads = model.alloc_grads();
-
-        // Initialise optimiser
-        let mut opt = Adam::new(
-            &model,
-            AdamConfig {
-                lr: 1e-3,
-                ..Default::default()
-            }
-        );
-
-        // Optimisation loop
-        for i in 0..50 {
-            // Collect the gradients of the network
-            let prediction = model.forward_mut(x.trace(grads));
-            let loss = mse_loss(prediction, x.clone());
-            println!("Loss after update {i}: {:?}", loss.array());
-            grads = loss.backward();
-
-            //  Update weights
-            opt.update(&mut model, &grads)
-                .expect("Oops, there were some unused params");
-            
-
-
-            model.zero_grads(&mut grads);
-        }
-
-        model
-    }
-}
-*/
-
-
 impl<const R: usize, const L: usize> AutoEncoder<R, L> {
     pub fn new(encoder: MLP<R, L>, decoder: MLP<L, R>) -> Self {
-        Self { encoder, decoder, model: None}
+        Self {
+            encoder,
+            decoder,
+            model: None,
+        }
     }
 
     /// Fit the model to the input data
-    pub fn fit(&mut self, x: Tensor<(usize, Const<R>), f32, Device>) {
+    pub fn fit(&mut self, x: Tensor<(usize, Const<R>), f32, Device>, epochs: usize, lr: f64) {
         // Initialise device
         let dev: Device = Device::default();
 
         // Define architecture
-        let arch = (self.encoder.clone(), self.decoder.clone());//self.ae_seq.clone();
+        let arch = (self.encoder.clone(), self.decoder.clone());
         let mut model = dev.build_module::<f32>(arch);
         let mut grads = model.alloc_grads();
 
@@ -115,37 +71,43 @@ impl<const R: usize, const L: usize> AutoEncoder<R, L> {
         let mut opt = Adam::new(
             &model,
             AdamConfig {
-                lr: 1e-3,
+                lr,
                 ..Default::default()
-            }
+            },
         );
 
         // Optimisation loop
-        for i in 0..50 {
+        for i in 0..epochs {
             // Collect the gradients of the network
             let prediction = model.forward_mut(x.trace(grads));
             let loss = mse_loss(prediction, x.clone());
             println!("Loss after update {i}: {:?}", loss.array());
             grads = loss.backward();
 
-            //  Update weights
+            // Update weights
             opt.update(&mut model, &grads)
                 .expect("Oops, there were some unused params");
-            
+
             model.zero_grads(&mut grads);
         }
 
         self.model = Some(model)
     }
 
-    pub fn encode(&self, x: Tensor<Rank1<R>, f32, Device>) -> Option<Tensor<Rank1<L>, f32, Device>> {
+    pub fn encode(
+        &self,
+        x: Tensor<Rank1<R>, f32, Device>,
+    ) -> Option<Tensor<Rank1<L>, f32, Device>> {
         match &self.model {
             Some((enc, _)) => Some(enc.forward(x)),
             None => None,
         }
     }
 
-    pub fn decode(&self, x: Tensor<Rank1<L>, f32, Device>) -> Option<Tensor<Rank1<R>, f32, Device>> {
+    pub fn decode(
+        &self,
+        x: Tensor<Rank1<L>, f32, Device>,
+    ) -> Option<Tensor<Rank1<R>, f32, Device>> {
         match &self.model {
             Some((_, dec)) => Some(dec.forward(x)),
             None => None,
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,57 @@
-use autoencoder::{Device, MLP, AutoEncoder};
+use std::{error::Error, fs::File};
+
+use autoencoder::{AutoEncoder, Device, MLP};
 
 use dfdx::{
     shapes::{Const, Rank1},
     tensor::{SampleTensor, Tensor},
 };
-
+use dfdx_core::tensor::TensorFrom;
 
 fn main() {
     // Initialise device
     let dev: Device = Device::default();
 
     // Define architecture
-    let mut ae: AutoEncoder<784, 10> = AutoEncoder::new(
-        MLP::new(400, 48),
-        MLP::new(48, 400)
+    let mut ae: AutoEncoder<784, 15> = AutoEncoder::new(
+        MLP::new(183, 48),
+        MLP::new(48, 183)
     );
 
-    // Initialise dummy data
-    let x = dev.sample_normal_like(&(100, Const::<784>));
+    // Initialise data
+    //let x = dev.sample_normal_like(&(100, Const::<784>));
+    let x: Tensor<(usize, Const<784>), f32, _> =
+        dev.tensor((read_mnist().ok().unwrap(), (10000, Const)));
 
     // Fit model
-    ae.fit(x);
+    ae.fit(x, 850, 5.0e-4);
 
     let x: Tensor<Rank1<784>, f32, _> = dev.sample_normal();
     let y = ae.encode(x).unwrap();
     println!("{:?}", y.as_vec())
-
 }
-/* 
-fn main() {
-    // Initialise device
-    let dev: Device = Device::default();
 
-    // Define architecture
-    let ae: AESequence<784, 10> = AESequence {
-        encoder: MLP::new(400, 48),
-        decoder: MLP::new(48, 400),
-    };
+fn read_mnist() -> Result<Vec<f32>, Box<dyn Error>> {
+    let file = File::open("mnist_test.csv")?;
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(file);
 
-    // Initialise dummy data
-    let x = dev.sample_normal_like(&(100, Const::<784>));
+    let pixel_values: Vec<f32> = rdr
+        .deserialize()
+        .filter_map(|record: Result<Vec<f32>, _>| match record {
+            Ok(record) => {
+                let mut vec: Vec<f32> = record.iter().map(|x| x / 255.).collect();
+                vec.remove(0);
+                Some(vec)
+            }
+            Err(e) => {
+                eprintln!("Error parsing record: {}", e);
+                None
+            }
+        })
+        .flatten()
+        .collect();
 
-    // Fit model
-    let model = ae.fit(x); 
-
-
-    let x: Tensor<Rank1<784>, f32, _> = dev.sample_normal();
-    let y = model.forward(x);
-    println!("{:?}", y.as_vec())
+    Ok(pixel_values)
 }
-*/
-
